eabc1dbaa7bad8b7038d2b236d1b224ff02c86ec
jonathan fernandez <jonathan.fvargas@gmail.com>
2017-08-09 20:26:23 -0500

    [JF] add enemies

--- js/main.js
+++ js/main.js
@@ -80,6 +80,10 @@ PlayState.preload = function () {
 
     //add coins
     this.game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
+    // spider enemy
+    this.game.load.spritesheet('spider', 'images/spider.png', 42, 32);
+    //invisible wall to spiders
+    this.game.load.image('invisible-wall', 'images/invisible_wall.png');
 
 };
 
@@ -100,15 +104,21 @@ PlayState._loadLevel = function (data) {
     this.platforms = this.game.add.group();
     //add coins to level
     this.coins = this.game.add.group();
+    //enemies
+    this.spiders = this.game.add.group();
+    //wall to avoid spider fall
+    this.enemyWalls = this.game.add.group();
 	console.log(data);
 	data.platforms.forEach(this._spawnPlatform, this);
     // spawn hero and enemies
-    this._spawnCharacters({hero: data.hero});
+    this._spawnCharacters({hero: data.hero, spiders: data.spiders});
     // enable gravity
     const GRAVITY = 1200;
     this.game.physics.arcade.gravity.y = GRAVITY;
     //spawn coins throught level
     data.coins.forEach(this._spawnCoin, this);
+    //set walls to non visible    
+    this.enemyWalls.visible = false;
 };
 
 PlayState._spawnCoin = function (coin) {
@@ -132,9 +142,30 @@ PlayState._spawnPlatform = function (platform) {
     this.game.physics.enable(sprite);
     sprite.body.allowGravity = false;
     sprite.body.immovable = true;
+
+    //invisible walls for spiders
+    this._spawnEnemyWall(platform.x, platform.y, 'left');
+    this._spawnEnemyWall(platform.x + sprite.width, platform.y, 'right');
+};
+
+PlayState._spawnEnemyWall = function (x, y, side) {
+    let sprite = this.enemyWalls.create(x, y, 'invisible-wall');
+    // anchor and y displacement
+    sprite.anchor.set(side === 'left' ? 1 : 0, 1);
+
+    // physic properties
+    this.game.physics.enable(sprite);
+    sprite.body.immovable = true;
+    sprite.body.allowGravity = false;
 };
 
 PlayState._spawnCharacters = function (data) {
+    // spawn spiders
+    data.spiders.forEach(function (spider) {
+        let sprite = new Spider(this.game, spider.x, spider.y);
+        this.spiders.add(sprite);
+    }, this);
+
     // spawn hero
     this.hero = new Hero(this.game, data.hero.x, data.hero.y);
     this.game.add.existing(this.hero);
@@ -151,6 +182,11 @@ PlayState._handleCollisions = function () {
     // want to check all coins, we can just pass null to indicate "no filter, please"
     this.game.physics.arcade.overlap(this.hero, this.coins, this._onHeroVsCoin,
         null, this);
+    //collisions for spiders
+    this.game.physics.arcade.collide(this.spiders, this.platforms);
+    //collisions for spiders and walls
+    this.game.physics.arcade.collide(this.spiders, this.platforms);
+    this.game.physics.arcade.collide(this.spiders, this.enemyWalls);
 };
 
 PlayState._onHeroVsCoin = function (hero, coin) {
@@ -168,5 +204,38 @@ PlayState._handleInput = function () {
         this.hero.move(1);
     }else{
         this.hero.move(0);
+    }
+};
+
+function Spider(game, x, y) {
+    Phaser.Sprite.call(this, game, x, y, 'spider');
+
+    // anchor
+    this.anchor.set(0.5);
+    // animation
+    this.animations.add('crawl', [0, 1, 2], 8, true);
+    this.animations.add('die', [0, 4, 0, 4, 0, 4, 3, 3, 3, 3, 3, 3], 12);
+    this.animations.play('crawl');
+
+    // physic properties
+    this.game.physics.enable(this);
+    this.body.collideWorldBounds = true;
+    this.body.velocity.x = Spider.SPEED;
+}
+
+Spider.SPEED = 100;
+
+// inherit from Phaser.Sprite
+Spider.prototype = Object.create(Phaser.Sprite.prototype);
+Spider.prototype.constructor = Spider;
+
+
+Spider.prototype.update = function () {
+    // check against walls and reverse direction if necessary
+    if (this.body.touching.right || this.body.blocked.right) {
+        this.body.velocity.x = -Spider.SPEED; // turn left
+    }
+    else if (this.body.touching.left || this.body.blocked.left) {
+        this.body.velocity.x = Spider.SPEED; // turn right
     }
 };
